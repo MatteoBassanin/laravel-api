5e3ae23d0a604077aa5368955e3e90e6f1aebed9
MatteoBassanin <matteo.bassanin@gmail.com>
2023-06-07 16:49:54 +0200

    sending email

---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,8 @@
+bBuongiorno Amministratore è stato contatto da :
+{{ $lead->name }}
+<br>
+con questa email :
+{{ $lead->email }}
+<br>
+e hanno lasciato il seguente messaggio :
+{{ $lead->message }}
