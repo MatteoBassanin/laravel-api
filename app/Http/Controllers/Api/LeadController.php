f01bb8544887973a82612bd7d3dc0ade1347b9c4
MatteoBassanin <matteo.bassanin@gmail.com>
2023-06-07 17:27:29 +0200

    validation on form email

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,6 +7,7 @@ use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
@@ -14,6 +15,23 @@ class LeadController extends Controller
 
         $data = $request->all();
 
+        $validator = Validator::make($data,
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'message' => 'required'
+            ]
+        );
+
+        if ($validator->fails()){
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ]
+                );
+        }
 
         $new_lead = new Lead();
         $new_lead->fill($data);
